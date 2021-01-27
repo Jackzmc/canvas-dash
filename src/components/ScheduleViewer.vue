d57533f6eee260eea43d6146714f664a9d755e92
Jackz <me@jackz.me>
2021-01-27 11:21:39 -0600

    Implement stored schedule in scheduleviewer

--- src/components/ScheduleViewer.vue
+++ src/components/ScheduleViewer.vue
@@ -30,7 +30,6 @@
 
 <script>
 
-import schedules from '@/data/schedule.json'
 const DAY_NAMES = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
 export default {
@@ -39,8 +38,24 @@ export default {
   name: 'ScheduleViewer',
   data() {
       return {
-
+          schedule: []
       }
+  },
+  created() {
+      if(window.localStorage.canvas_schedule)
+        this.schedule = JSON.parse(window.localStorage.canvas_schedule)
+        .map(entry => {
+            if(entry.course && entry.course !== "none") {
+                const classData = this.courses.find(c => c.id == entry.course)
+                if(!classData) {
+                    console.warn(`Could not find a canvas course set for entry`, entry)
+                    return entry;
+                }
+                const cleanName = classData.name.split(" ").slice(0, 2).join(" ")
+                return {...entry, course: classData, cleanName, hasCanvasData: true, type: "COURSE"}
+            } else
+                return entry;
+        })
   },
   computed: {
       todayName() {
@@ -50,36 +65,6 @@ export default {
           const day = new Date().toString().slice(0,1).toUpperCase();
           if(day == "S") return "M";
           return day;
-      },
-      coursesComplete() {
-          return schedules.courses
-          .map(entry => {
-              if(!entry.id) return entry;
-              const classData = this.courses.find(v => v.id === entry.id)
-              if(!classData) {
-                console.warn(`Schedule entry's id not found on canvas`, entry)
-                return entry;
-              }
-              const cleanName = classData.name.split(" ").slice(0, 2).join(" ")
-              return {...entry, ...classData, cleanName, hasCanvasData: true, type: "COURSE"}
-          }).filter(v => v)
-      },
-      schedule() {
-          const personal = schedules.personal.map(entry => {
-              return {...entry, id: entry.name + entry.days.toString()}
-          })
-          return [...this.coursesComplete, ...personal].map(entry => {
-              //get start as timestamp to sort
-              const [time, period] = entry.starts.split(" ")
-              const [hours, minutes] = time.split(":")
-              const startTime = new Date();
-              if(period === "PM") 
-                  startTime.setHours(hours + 12)
-              else
-                startTime.setHours(hours)
-              if(minutes) startTime.setMinutes(minutes)
-              return {...entry, startTime: startTime.valueOf() }
-          }).sort((a,b) => a.startTime - b.startTime)
       },
       todaysSchedule() {
           return this.schedule.filter(v => v.days.includes(this.today)).map(entry => {
